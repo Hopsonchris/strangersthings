a9470b396a42d9fabed558160b3c63c3d57317f4
Chris Hopson <hopsonchristopher@gmail.com>
2022-09-10 03:14:33 -0500

    Later Friday night

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Stranger's Things!</h1>
       <button>
-        <Link to="/posts/create-post">Add a Post</Link>
+        <Link to="/posts/create-post">
+          <Button variant="contained">Add a Post</Button>
+        </Link>
       </button>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,45 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
 const Navbar = ({ logout, token }) => {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/Posts">Posts</Link>
-        <Link to="/Profile">Profile</Link>
+        <Link to="/">
+          <Button href="/" variant="contained">
+            Home
+          </Button>
+        </Link>
+        <Link to="/Posts">
+          <Button href="/" variant="contained">
+            Posts
+          </Button>
+        </Link>
+        <Link to="/Profile">
+          <Button href="/" variant="contained">
+            Profile
+          </Button>
+        </Link>
 
         {token ? (
           <Link to="/" onClick={() => logout()}>
-            Logout
+            <Button href="/" variant="contained">
+              Logout
+            </Button>
           </Link>
         ) : (
           <Fragment>
-            <Link to="/Register">Register</Link>
-            <Link to="/Login">Login</Link>
+            <Link to="/Register">
+              <Button href="/" variant="contained">
+                Register
+              </Button>
+            </Link>
+            <Link to="/Login">
+              <Button href="/" variant="contained">
+                Login
+              </Button>
+            </Link>
           </Fragment>
         )}
       </nav>
